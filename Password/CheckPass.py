2ed893c585841668036a0c2ce2baac99cfb9ff88
unknown <airas.latypov@gmail.com>
2023-04-27 23:17:28 +0500

    Functions

--- Password/CheckPass.py
+++ Password/CheckPass.py
@@ -9,44 +9,44 @@
 # Если пароль прошел все проверки, функция должна вывести на экран фразу "Perfect password", в противном случае - "Easy peasy".
 
 
-def check_password(пароль):
-    numbers = check_numbers(data=пароль)
+def check_password(пароль):                                     # Создаем функцию на которую будут ссылаться остальные функции.
+    numbers = check_numbers(data=пароль)                        # Переменные ссылаются на остальные функции.
     upper = check_upper(data=пароль)
     symbol = check_symbol(data=пароль)
     count = check_count(data=пароль)
 
-    # print(f"Numbers:   {numbers}")
+    # print(f"Numbers:   {numbers}")                            # Проверочная распечатка функций.
     # print(f"Upper:     {upper}")
     # print(f"Symbol:    {symbol}")
     # print(f"lenght:    {count}")
 
-    if (numbers and upper and symbol and count) is True:
+    if (numbers and upper and symbol and count) is True:        # Условие (функция должна вывести на экран фразу "Perfect password", в противном случае - "Easy peasy").
         print("Perfect password")
     else:
         print("Easy peasy")
 
-def check_numbers(data):
+def check_numbers(data):                                        # Создаем функцию на проверку присутствия цифр в пароле. Должно быть 3 в пароле.
     list_number = []
     for element in data:
-        if element.isdigit():
-            list_number.append(element)
+        if element.isdigit():                                   # .isdigit() возвращает цифры из пароля.                  
+            list_number.append(element)                         # Добавляем найденную цифру в "list_number = []"
     if len(list_number) >= 3:
         return True
     else:
         return False
 
-def check_upper(data):
+def check_upper(data):                                          # Создаем функцию на проверку присутствия заглавных букв. Должен быть хотя бы 1 в пароле.
     list_upper = []
     for символ in data:
-        if символ.isupper():
-            list_upper.append(символ)
+        if символ.isupper():                                    # .isupper() возвращает заглавные буквы из пароля.
+            list_upper.append(символ)                           # Добавляем найденную заглавную букву в "list_upper = []"
     if len(list_upper) >= 1:
         return True
     else:
         return False
 
-def check_symbol(data):
-    znaki = "!@#$%*"
+def check_symbol(data):                                          # Создаем функцию на проверку присутствия знаков "!@#$%*". Должен быть хотябы 1 в пароле.
+    znaki = "!@#$%*"                                             # Создаем переменную со знаками "!@#$%*"
     list_znaki = []
     for symbol in data:
         if symbol in znaki:
@@ -56,7 +56,7 @@ def check_symbol(data):
     else:
         return False
 
-def check_count(data):
+def check_count(data):                                          # СОздаем функцию на проверку колличества символов в пароле. Не меньше 10ти должно быть.
     summa = len(data)
     if summa >= 10:
         return True
@@ -64,4 +64,4 @@ def check_count(data):
         return False
 
 
-check_password(пароль="password1223QA!Z")
+check_password(пароль="password1223QA!Z")                       # Вводим пароль на проверку.
